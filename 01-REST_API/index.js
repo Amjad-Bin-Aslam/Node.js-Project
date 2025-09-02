c536c2b681b2146cdf6eb720ef63b07b8df00910
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-02 14:54:30 +0500

    compeleted

--- 01-REST_API/index.js
+++ 01-REST_API/index.js
@@ -1,19 +1,21 @@
 const express =  require('express')
+const fs = require("fs")
 const users = require("./MOCK_DATA.json")
 
 const PORT = 8000;
 const app = express();
 
+// url Plugin
+app.use(express.urlencoded({ extended: false }))
 
-
-// app.get('/users' , (req , res) => {
-//     const html = `
-//         <ul>
-//             ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
-//         </ul>
-//     `;
-//     res.send(html)
-// })
+app.get('/users' , (req , res) => {
+    const html = `
+        <ul>
+            ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
+        </ul>
+    `;
+    res.send(html)
+})
 
 // Routes
 app.get('/api/users' , (req , res) => {
@@ -31,17 +33,36 @@ app.route("/api/users/:id")
     return res.json(user)
 })
 .patch((req , res) => {
-    //Edit user
+    //Edit user(
     res.json({status: "pending"})
 })
 .delete((req , res) => {
     //Delete user
-    res.json({status: "pending"})
-})
+    const id = Number(req.params.id)
+    const user = users.findIndex((user) => user.id  === id)
+    if(user === -1){
+        return res.status(404).json({status: "error" , message: "user not found"})
+    }
+
+    users.splice(user ,  1);
+
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users) , (err) => {
+        if(err){
+            return res.status.json({status: "error" , message: "Failed to delete user"})
+        }
+        return res.json({status: "Success" , message: "User Deleted successfully"})
+    });
+    
+});
 
 app.post('/api/users' , (req , res) => {
     // Create user
-    return res.json({status: "pending"})
+    const body = req.body;
+    users.push({...body , id:users.length + 1});
+    fs.writeFile("./MOCK_DATA.json" , JSON.stringify(users) , (err , data) => {
+        return res.json({status: "success"})
+    })
+    
 })
 
 
