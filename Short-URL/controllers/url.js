a2dff4966283e7284c664e77ef5ae9d024cc0344
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-04 23:46:18 +0500

    aded

--- Short-URL/controllers/url.js
+++ Short-URL/controllers/url.js
@@ -2,9 +2,11 @@ const {nanoids, nanoid} = require('nanoid')
 const URL = require('../models/url')
 
 async function handleGenerateNewShortURL(req , res) {
-    const shortID = nanoid(8);
+    
     const body = req.body;
     if(!body.url) return res.status(400).json({ error: 'url required'})
+    const shortID = nanoid(8);
+        
     await URL.create({
         shortId: shortID,
         redirectURL: body.url,
@@ -14,6 +16,15 @@ async function handleGenerateNewShortURL(req , res) {
 };
 
 
+async function handleGetAnalytics(req , res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOneAndUpdate({
+        shortId
+    })
+    return res.json({ totalClicks: result.visitHistory.length , analytics: result.visitHistory})
+}
+
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics, 
 }
--- Short-URL/index.js
+++ Short-URL/index.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const {connectToMongoDB} = require('./connect')
+const urlRoute = require('./routes/url')
+const URL = require('./models/url')
+const path = require('path')
+const staticRoute = require('./routes/staticRouter')
 
-const urlRouote = require('./routes/url')
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(() => console.log("MongoDB connected")
 );
 
-app.use(express.json());
+app.set("view engine" , "ejs");
+app.set("views" , path.resolve('./views'))
 
-app.use('/url' , urlRouote)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/url' , urlRoute);
+app.use('/' , staticRoute);
+
+
+
+app.get('/url/:shortId' , async (req , res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+        shortId
+        },
+        { 
+        $push: {
+        visitHistory: {
+            timestamp: Date.now()
+          },
+        },
+      }
+    );
+    res.redirect(entry.redirectURL);
+});
+
+
+
 
 
 app.listen(PORT , () => console.log("Server Started at PORT: " , PORT))
